398c31e0b42c9fa82cef4b7d169f6c7fb26a03d9
ALMSantana <andrelms4@insper.edu.br>
2025-05-24 16:18:49 -0300

    Scripts del aula 4

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from time import sleep
 from utils import carga, guarda
 from personas import personas, seleccionar_persona
+from gestion_historial import eliminar_mensajes_antiguos
 
 load_dotenv()
 
@@ -17,6 +18,43 @@ app.secret_key = 'aluralatam'
 
 contexto = carga("datos/musicmart.txt")
 
+def crear_chatbot():
+    personalidad = "neutro"
+    prompt_sistema = f"""
+                        # PERSONA
+
+                        Eres un chatbot de atención al cliente de una e-commerce. No debes
+                        responder preguntas que no sean referentes a los datos del ecommerce 
+                        informado.
+
+                        Únicamente debes de utilizar los datos que estén dentro del 'contexto'.
+
+                        # CONTEXTO
+                        {contexto}
+
+                        # PERSONALIDAD
+                        {personalidad}
+
+                        # HISTORIAL
+                        Accede siempre al historial de mensajes, y recupera las informaciones previamente
+                        mencionadas.
+                    """
+    configuracion_modelo = {
+        "temperature":0.2,
+        "max_output_tokens": 8192
+    }
+
+    llm = genai.GenerativeModel(
+        model_name = modelo,
+        system_instruction = prompt_sistema,
+        generation_config = configuracion_modelo   
+    )    
+
+    chatbot = llm.start_chat(history=[]) 
+    return chatbot
+
+chatbot = crear_chatbot()
+
 def bot(prompt):
     #número máximo de intentos 
     max_intentos = 1
@@ -24,34 +62,17 @@ def bot(prompt):
     while True:
         try:
             personalidad = personas[seleccionar_persona(prompt)]
-            prompt_sistema = f"""
-                                # PERSONA
+            mensaje_usuario = f"""
+                                 Considera esta personalidad para responder al mensaje:
+                                 {personalidad}.
 
-                                Eres un chatbot de atención al cliente de una e-commerce. No debes
-                                responder preguntas que no sean referentes a los datos del ecommerce 
-                                informado.
-
-                                Únicamente debes de utilizar los datos que estén dentro del 'contexto'.
-
-                                # CONTEXTO
-                                {contexto}
-
-                                # PERSONALIDAD
-                                {personalidad}
-                             """
-            
-            configuracion_modelo = {
-                "temperature":0.2,
-                "max_output_tokens": 8192
-            }
-
-            llm = genai.GenerativeModel(
-                model_name = modelo,
-                system_instruction = prompt_sistema,
-                generation_config = configuracion_modelo   
-            )
-
-            respuesta = llm.generate_content(prompt)
+                                 Responde al mensaje siguiente, siempre recordando el historial:
+                                 {prompt}
+                               """
+            respuesta = chatbot.send_message(mensaje_usuario)
+            if len(chatbot.history) > 4:
+                chatbot.history = eliminar_mensajes_antiguos(chatbot.history)
+            print(f'Cantidad de mensajes: {len(chatbot.history)}\n{chatbot.history}') 
             return respuesta.text
         
         except Exception as e:
