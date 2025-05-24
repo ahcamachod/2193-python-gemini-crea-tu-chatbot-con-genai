29bda59de10bbb3a113efe06b2cdaadf34eaba99
ALMSantana <andrelms4@insper.edu.br>
2025-05-24 12:05:35 -0300

    Script del aula 2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request, Response
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+from time import sleep
 
 load_dotenv()
 
@@ -11,6 +12,42 @@ genai.configure(api_key=api_key)
 
 app = Flask(__name__)
 app.secret_key = 'aluralatam'
+
+def bot(prompt):
+    #número máximo de intentos 
+    max_intentos = 1
+    repeticion = 0
+    while True:
+        try:
+            prompt_sistema = """Eres un chatbot de atención al cliente de una e-commerce. No debes
+                                responder preguntas que no sean referentes a los datos del ecommerce 
+                                informado."""
+            
+            configuracion_modelo = {
+                "temperature":0.2,
+                "max_output_tokens": 8192
+            }
+
+            llm = genai.GenerativeModel(
+                model_name = modelo,
+                system_instruction = prompt_sistema,
+                generation_config = configuracion_modelo   
+            )
+
+            respuesta = llm.generate_content(prompt)
+            return respuesta.text
+        
+        except Exception as e:
+            repeticion += 1
+            if repeticion >= max_intentos:
+                return "Error con Gemini: %s" % e
+            sleep(50)
+
+@app.route("/chat", methods=["POST"]) #Estamos creando la ruta hacia el endpoint /chat que va a invocar el metodo POST 
+def chat():
+    prompt = request.json["msg"]
+    respuesta = bot(prompt)
+    return respuesta
 
 @app.route("/")
 def home():
